920398ef8cd143f8b713cdcbc8b35e960d902ac5
mswgen <54498184+mswgen@users.noreply.github.com>
2020-02-29 12:14:36 +0900

    Delete start.js

--- start.js
+++
@@ -1,9 +0,0 @@
-const { ShardingManager } = require('discord.js');
-require("dotenv").config({ path: __dirname + "/config.json" })
-const manager = new ShardingManager('./bot.js', {
-    token: Token
-});
-
-manager.spawn();
-manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
-manager.on('shardCreate', shard => console.log(`Create shard ${shard.id}`));
